f7075935d54d8ebae155bcc6c977a4a6e73ad772
paul <1210055099@qq.com>
2019-09-20 18:15:19 +0800

    1、调整wxpython demo，增加颜色显示 2、在grid_demo2中增加计时器功能

--- Experiment/wxpythonGUI/MyCode/Data_Pro_Sub.py
+++ Experiment/wxpythonGUI/MyCode/Data_Pro_Sub.py
@@ -1,7 +1,9 @@
 # encoding=utf-8
 from Config.Sub import readConfig
 from DataSource.Data_Sub import get_k_data_JQ
-from SDK.Gen_Stk_Index_Pic_Sub import gen_Hour_MACD_Pic, gen_Day_Pic, gen_W_M_MACD_Pic, gen_Idx_Pic
+from SDK.Gen_Stk_Pic_Sub import gen_Hour_MACD_Pic, gen_Day_Pic, gen_W_M_MACD_Pic, gen_Idx_Pic, gen_Hour_MACD_Pic_wx, \
+    gen_Day_Pic_wx, gen_W_M_MACD_Pic_wx, gen_Idx_Pic_wx
+from pylab import *
 
 
 def get_pic_dict():
@@ -23,10 +25,10 @@ def get_pic_dict():
         for stk in stk_list:
             df = get_k_data_JQ(stk, 400)
             stk_pic_dict = {
-                'hour': gen_Hour_MACD_Pic(stk),
-                'day': gen_Day_Pic(df, stk_code=stk)[0],
-                'wm': gen_W_M_MACD_Pic(stk),
-                'index': gen_Idx_Pic(df, stk_code='')[0]
+                'hour': gen_Hour_MACD_Pic_wx(stk),
+                'day': gen_Day_Pic_wx(df, stk_code=stk),
+                'wm': gen_W_M_MACD_Pic_wx(stk),
+                'index': gen_Idx_Pic_wx(df, stk_code='')
             }
 
             stk_list_pic_dict[stk] = stk_pic_dict
