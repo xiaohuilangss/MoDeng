04df40eb54f36e92dd1c07bc09c7b88764d1ba9a
paul <189916591@qq.com>
2019-12-07 16:25:38 +0800

    1、修复命令行启动的问题

--- modeng.py
+++ modeng.py
@@ -1,4 +1,22 @@
 # encoding=utf-8
+""" =========================== 将当前路径及工程的跟目录添加到路径中，必须在文件头部，否则易出错 ============================ """
+
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+if "MoDeng" in curPath:
+	rootPath = curPath[:curPath.find("MoDeng\\") + len("MoDeng\\")]  # 获取myProject，也就是项目的根路径
+elif "MoDeng-master" in curPath:
+	rootPath = curPath[:curPath.find("MoDeng-master\\") + len("MoDeng-master\\")]  # 获取myProject，也就是项目的根路径
+else:
+	print('没有找到项目的根目录！请检查项目根文件夹的名字！')
+	exit(1)
+
+sys.path.append('..')
+sys.path.append(rootPath)
+
+
+
 import multiprocessing as mp
 
 from Function.GUI.GUI_main.Sub import run_myframe_in_process
