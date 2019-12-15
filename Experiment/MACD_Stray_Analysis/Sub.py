d593501fbcffdf6be6b68d47250e832178b7fcfb
paul <189916591@qq.com>
2019-12-15 21:40:52 +0800

    1、上传安装手册pdf版本

--- Experiment/MACD_Stray_Analysis/Sub.py
+++ Experiment/MACD_Stray_Analysis/Sub.py
@@ -19,106 +19,11 @@ from pylab import *
 from SDK.MyTimeOPT import get_current_date_str, add_date_str
 
 
-# def plot_W_M(df_w, df_m):
-#     """
-#
-#     :param df_w:
-#     :param df_m:
-#     :return:
-#     """
-#     """ --------------------------------------- 生成图片 -------------------------------------"""
-#     fig, ax = subplots(ncols=1, nrows=4)
-#
-#     ax[0].plot(range(0, len(df_w['date'])), df_w['close'], 'g*--', label='close')
-#     ax[1].bar(range(0, len(df_w['date'])), df_w['MACD'],  label='week_MACD')
-#     ax[1].plot(range(0, len(df_w['date'])), [0 for x in range(0, len(df_w['date']))], 'r--', label='week_MACD')
-#
-#     ax[2].plot(range(0, len(df_m['date'])), df_m['close'], 'g*--', label='close')
-#     ax[3].bar(range(0, len(df_m['date'])), df_m['MACD'],  label='month_MACD')
-#     ax[3].plot(range(0, len(df_m['date'])), [0 for x in range(0, len(df_m['date']))], 'r--', label='month_MACD')
-#
-#     return fig, ax
-
-
-def send_W_M_MACD(stk_code, towho):
-    """
-
-    :param stk_code:
-    :param towho:
-    :return:
-    """
-
-    # 获取今天的情况，涨幅没有超过3%的不考虑
-    # df_now = get_k_data_JQ(stk_code, count=2, end_date=get_current_date_str()).reset_index()
-    #
-    # if (df_now.tail(1)['close'].values[0]-df_now.head(1)['close'].values[0])/df_now.head(1)['close'].values[0] < 0.03:
-    #     print('函数week_MACD_stray_judge：' + stk_code + '涨幅不够！')
-    #     return False
-
-    df = get_k_data_JQ(stk_code, count=400, end_date=get_current_date_str()).reset_index()
-
-    if len(df) < 350:
-        print('函数week_MACD_stray_judge：'+stk_code + '数据不足！')
-        return False
-
-    # 规整
-    df_floor = df.tail(math.floor(len(df)/20)*20-19)
-
-    # 增加每周的星期几
-    df_floor['day'] = df_floor.apply(
-        lambda x: calendar.weekday(int(x['date'].split('-')[0]), int(x['date'].split('-')[1]),
-                                   int(x['date'].split('-')[2])), axis=1)
-
-    # 增加每周的星期几
-    df_floor['day'] = df_floor.apply(lambda x: calendar.weekday(int(x['date'].split('-')[0]), int(x['date'].split('-')[1]), int(x['date'].split('-')[2])), axis=1)
-
-    # 隔着5个取一个
-    if df_floor.tail(1)['day'].values[0] != 4:
-        df_floor_slice_5 = pd.concat([df_floor[df_floor.day == 4], df_floor.tail(1)], axis=0)
-    else:
-        df_floor_slice_5 = df_floor[df_floor.day == 4]
-
-    # 获取最后的日期
-    date_last = df_floor_slice_5.tail(1)['date'].values[0]
-
-    # 计算指标
-    df_floor_slice_5['MACD'], df_floor_slice_5['MACDsignal'], df_floor_slice_5['MACDhist'] = talib.MACD(df_floor_slice_5.close,
-                                                                          fastperiod=6, slowperiod=12,
-                                                                          signalperiod=9)
-
-    # 隔着20个取一个（月线）
-    df_floor_slice_20 = df_floor.loc[::20, :]
-
-    # 计算指标
-    df_floor_slice_20['MACD'], df_floor_slice_20['MACDsignal'], df_floor_slice_20['MACDhist'] = talib.MACD(
-        df_floor_slice_20.close,
-        fastperiod=4,
-        slowperiod=8,
-        signalperiod=9)
-
-    """ --------------------------------------- 生成图片 -------------------------------------"""
-    fig, ax = plot_W_M(df_floor_slice_5, df_floor_slice_20)
-
-    # 增加标题
-    plt.title(stk_code + 'month-stray' + date_last)
-
-    # 发送图片
-    send_pic_qq(towho, fig)
-
-    # 关闭图片
-    plt.close()
-
-
-def week_MACD_stray_judge(stk_code, towho, debug_plot=False):
+def week_macd_stray_judge(stk_code):
 
     try:
         # 获取今天的情况，涨幅没有超过3%的不考虑
         df_now = get_k_data_JQ(stk_code, count=2, end_date=get_current_date_str()).reset_index()
-
-        if (df_now.tail(1)['close'].values[0]-df_now.head(1)['close'].values[0])/df_now.head(1)['close'].values[0] < -0.05:
-            print('函数week_MACD_stray_judge：' + stk_code + '涨幅不够！')
-            return False, pd.DataFrame()
-
         df = get_k_data_JQ(stk_code, count=400, end_date=get_current_date_str()).reset_index()
 
         if len(df) < 350:
@@ -164,36 +69,17 @@ def week_MACD_stray_judge(stk_code, towho, debug_plot=False):
         MACD_5 = df_floor_slice_5.tail(3)['MACD'].values
         MACD_20 = df_floor_slice_20.tail(4)['MACD'].values
         if (MACD_5[1] == np.min(MACD_5)) & (MACD_20[1] != np.max(MACD_20)) & (MACD_20[2] != np.max(MACD_20)):
-
-            if debug_plot:
-                """ --------------------------------------- 生成图片 -------------------------------------"""
-                fig, ax = plot_W_M(df_floor_slice_5, df_floor_slice_20)
-
-                # 增加标题
-                plt.title(stk_code + 'month-stray' + date_last)
-
-                # 发送图片
-                send_pic_qq(towho, fig)
-
-                # 关闭图片
-                plt.close()
             return True, df
         else:
             return False, pd.DataFrame()
     except Exception as e:
-        send_qq(towho, stk_code + '出错：\n' + str(e))
+        print('函数week_MACD_stray_judge：出错！原因：\n' + str(e))
         return False, pd.DataFrame()
 
 
 def stk_sea_select(stk_code, towho, tc, debug_plot=False):
 
     try:
-        # 获取今天的情况，涨幅没有超过3%的不考虑
-        # df_now = get_k_data_JQ(stk_code, count=2, end_date=get_current_date_str()).reset_index()
-        #
-        # if (df_now.tail(1)['close'].values[0]-df_now.head(1)['close'].values[0])/df_now.head(1)['close'].values[0] < -0.05:
-        #     print('函数week_MACD_stray_judge：' + stk_code + '涨幅不够！')
-        #     return False, pd.DataFrame()
 
         """ ------------------------ 下载原始数据 ------------------------------- """
         df = get_k_data_JQ(stk_code, count=400, end_date=get_current_date_str()).reset_index()
@@ -303,7 +189,55 @@ def cal_stk_p_level(c):
     }
 
 
-def checkWeekStrayForAll():
+def filter_by_week_stray(stk_basics):
+    """
+    根据 周线进行筛选
+    :param stk_basics:
+    :return:
+    """
+    week_flag = list(stk_basics.reset_index().apply(lambda x: (x['code'], week_macd_stray_judge(x['code'])), axis=1))
+    return stk_basics[week_flag]
+
+
+def filter_by_age(stk_basics, age_min=None, age_max=None):
+
+    """
+    根据股票的上市时间进行筛选, 以年为单位
+    :param df_total: tushare ts.get_stock_basics() 返回值
+    :param age_min:
+    :param age_max:
+    :return:
+    """
+    df_total = stk_basics.deepcopy()
+
+    if age_min is not None:
+
+        # 过滤掉年龄过小的
+        df_total = df_total[
+            df_total.apply(lambda x: int(str(x['timeToMarket'])[:4]) <= int(get_current_date_str()[:4]) - age_min, axis=1)]
+
+    if age_max is not None:
+        # 过滤掉年龄过大的
+        df_total = df_total[
+            df_total.apply(lambda x:int(get_current_date_str()[:4]) - int(str(x['timeToMarket'])[:4]) <= age_max, axis=1)]
+
+    return df_total
+
+
+def filter_by_ratio(stk_basics, days_span, limit_ratio_min=None, limit_ratio_max=None):
+    """
+    根据价格在设定区间内的水平来
+    :param stk_basics:
+    :param days_span:
+    :param limit_ratio:
+    :return:
+    """
+
+    # 增加stk的水平信息
+    df_level = [(x[0], x[1][1], cal_stk_p_level(x[1][1]['close'].values)) for x in df_age_f2]
+
+
+def check_week_stray_for_all():
 
     towho = '影子2'
     send_qq(towho, '以下是今晚海选结果：')
@@ -314,7 +248,7 @@ def checkWeekStrayForAll():
     df_age_filter = df_total[df_total.apply(lambda x: int(str(x['timeToMarket'])[:4]) <= int(get_current_date_str()[:4]) - 4, axis=1)]
 
     # 根据week反转情况进行过滤，保留有反转的单位
-    df_age_filter_stray = list(df_age_filter.reset_index().apply(lambda x: (x['code'], week_MACD_stray_judge(x['code'], towho)), axis=1))
+    df_age_filter_stray = list(df_age_filter.reset_index().apply(lambda x: (x['code'], week_macd_stray_judge(x['code'], towho)), axis=1))
 
     # 过滤掉非反转的情况
     df_age_f2 = list(filter(lambda x: x[1][0], df_age_filter_stray))
@@ -360,61 +294,10 @@ def checkWeekStrayForAll():
 if __name__ == '__main__':
 
     from DataSource.auth_info import *
+    jq_login()
 
-    # week_MACD_stray_judge('603421', '影子')
+    df_total = ts.get_stock_basics()
 
-
-    checkWeekStrayForAll()
-    send_W_M_MACD('000001', '影子')
-
-    r = calendar.weekday(2019, 12, 5)
-    # week_MACD_stray_judge('300508', '影子')
-
-    checkWeekStrayForAll()
-
-    stk_code = '600707'
-    df = get_k_data_JQ(stk_code, count=600, end_date=get_current_date_str()).reset_index()
-
-    # 规整
-    df_floor = df.tail(math.floor(len(df)/20)*20-19)
-
-    # 取均值
-    df_floor['close_m5'] = df_floor['close'].rolling(window=5).mean()
-
-    # 增加每周的星期几
-    df_floor['day'] = df_floor.apply(lambda x: calendar.weekday(int(x['date'].split('-')[0]), int(x['date'].split('-')[1]), int(x['date'].split('-')[2])), axis=1)
-
-    # 隔着5个取一个
-    # df_floor_slice_5 = df_floor.loc[::5, :]
-    if df_floor.tail(1)['day'].values[0] != 4:
-        df_floor_slice_5 = pd.concat([df_floor[df_floor.day==4], df_floor.tail(1)], axis=0)
-    else:
-        df_floor_slice_5 = df_floor[df_floor.day==4]
-
-    # 计算指标
-    df_floor_slice_5['MACD'], df_floor_slice_5['MACDsignal'], df_floor_slice_5['MACDhist'] = talib.MACDEXT(df_floor_slice_5.close,
-                                                                          fastperiod=6, slowperiod=12,
-                                                                          signalperiod=9)
-
-    df_floor_slice_5['MACD'] = df_floor_slice_5.apply(lambda x: 2*x['MACD'], axis=1)
-
-    df_floor_slice_5['close_M26'] = df_floor_slice_5['close'].rolling(window=26).mean()
-    df_floor_slice_5['close_M12'] = df_floor_slice_5['close'].rolling(window=12).mean()
-
-    df_floor_slice_5['diff'] = df_floor_slice_5.apply(lambda x: x['close_M12'] - x['close_M26'], axis=1)
-    df_floor_slice_5['diff_M9'] = df_floor_slice_5['diff'].rolling(window=9).mean()
-
-    df_floor_slice_5['myMACD'] = df_floor_slice_5.apply(lambda x: 2*(x['diff'] - x['diff_M9']), axis=1)
-
-
-    checkWeekStrayForAll()
-
-
-
-
-    """
-    画图
-    df_floor_slice_5.plot('date', ['close', 'MACD', 'myMACD'], subplots=True, style=['--*', '*', '*'])
-    """
+    time
 
     end = 0
