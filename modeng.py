a547328885f4f0974e62025f6ae1fe2c7243fac5
paul <1210055099@qq.com>
2019-12-16 18:40:56 +0800

    1、打包资料上传

--- modeng.py
+++ modeng.py
@@ -2,45 +2,45 @@
 
 if __name__ == '__main__':
 
-	try:
+	# try:
 
-		""" =========================== 将当前路径及工程的跟目录添加到路径中，必须在文件头部，否则易出错 ============================ """
+	""" =========================== 将当前路径及工程的跟目录添加到路径中，必须在文件头部，否则易出错 ============================ """
 
-		import sys
-		import os
+	import sys
+	import os
 
-		curPath = os.path.abspath(os.path.dirname(__file__))
-		if "MoDeng" in curPath:
-			rootPath = curPath[:curPath.find("MoDeng\\") + len("MoDeng\\")]  # 获取myProject，也就是项目的根路径
-		elif "MoDeng-master" in curPath:
-			rootPath = curPath[:curPath.find("MoDeng-master\\") + len("MoDeng-master\\")]  # 获取myProject，也就是项目的根路径
-		else:
-			input('没有找到项目的根目录！请检查项目根文件夹的名字！')
-			exit(1)
+	curPath = os.path.abspath(os.path.dirname(__file__))
+	if "MoDeng" in curPath:
+		rootPath = curPath[:curPath.find("MoDeng\\") + len("MoDeng\\")]  # 获取myProject，也就是项目的根路径
+	elif "MoDeng-master" in curPath:
+		rootPath = curPath[:curPath.find("MoDeng-master\\") + len("MoDeng-master\\")]  # 获取myProject，也就是项目的根路径
+	else:
+		input('没有找到项目的根目录！请检查项目根文件夹的名字！')
+		exit(1)
 
-		sys.path.append('..')
-		sys.path.append(rootPath)
+	sys.path.append('..')
+	sys.path.append(rootPath)
 
-		import multiprocessing as mp
+	import multiprocessing as mp
 
-		from Function.GUI.GUI_main.Sub import run_myframe_in_process
-		from Function.GUI.GUI_main.Thread_Sub import data_process_callback
-		from Config.AutoGenerateConfigFile import checkConfigFile
+	from Function.GUI.GUI_main.Sub import run_myframe_in_process
+	from Function.GUI.GUI_main.Thread_Sub import data_process_callback
+	from Config.AutoGenerateConfigFile import checkConfigFile
 
-		# 检查配置文件
-		checkConfigFile()
+	# 检查配置文件
+	checkConfigFile()
 
-		# 定义管道
-		pipe_master, pipe_proc = mp.Pipe()
+	# 定义管道
+	pipe_master, pipe_proc = mp.Pipe()
 
-		# 启动主进程
-		process = mp.Process(target=run_myframe_in_process, args=(pipe_master, False))
-		process.start()
+	# 启动主进程
+	process = mp.Process(target=run_myframe_in_process, args=(pipe_master, False))
+	process.start()
 
-		# 启动处理循环
-		data_process_callback(pipe_proc, debug=False)
+	# 启动处理循环
+	data_process_callback(pipe_proc, debug=False)
 
-	except Exception as e:
-		print('出错！错误：\n' + str(e) + '\n')
+	# except Exception as e:
+		# print('出错！错误：\n' + str(e) + '\n')
 
-		a = input('请关闭！')
+		# a = input('请关闭！')
---
+++ 文档/打包资料/打包欠缺/jqdatasdk/jqdata.thrift
@@ -0,0 +1,25 @@
+
+enum DataType {
+    Serial = 1,
+    DataFrame = 2,
+    Panel = 3
+}
+
+struct St_Query_Rsp {
+    1:required bool status,
+    2:optional string msg,
+    5:optional string error
+}
+
+struct St_Query_Req {
+    1:required string method_name,
+    2:required binary params,
+}
+
+service JqDataService {
+    St_Query_Rsp query(1:St_Query_Req rsp),
+    St_Query_Rsp auth(1:string username, 2:string password, 5:bool compress, 8:string mac),
+    St_Query_Rsp auth_by_token(1: string token)
+}
+
+
