fcb3add022351892abd07d0476a05a2992b34ca1
paul <189916591@qq.com>
2020-01-01 21:56:59 +0800

    Merge branch 'master' into develop
    # Conflicts:
    #	.idea/MoDeng.iml
    #	.idea/misc.xml
    #	Config/GlobalSetting.py
    #	Experiment/Capital_Flow/Money_Flow.py
    #	Experiment/CornerDetectAndAutoEmail/Sub.py
    #	Experiment/RelativeRank/Sub.py
    #	Function/GUI/GUI_main/Sub.py
    #	Function/LSTM/AboutLSTM/Test/DoPredict_2.py
    #	Function/LSTM/AboutLSTM/Test/LSTM_Book.py
    #	Function/LSTM/AboutLSTM/Test/TomorrowPredict.py
    #	Function/SeaSelect/SingleStock.py
    #	Function/SeaSelect/Sub/Sub.py
    #	Function/SeaSelect/gen_pic.py
    #	README.md
    #	SDK/DataPro.py
    #	SDK/Gen_Stk_Pic_Sub.py
    #	modeng.py
    #	使用说明书.txt

--- Function/SeaSelect/Sub/Sub.py
+++ Function/SeaSelect/Sub/Sub.py
@@ -7,13 +7,64 @@ import tushare as ts
 
 from DataSource.Code2Name import code2name
 from DataSource.Data_Sub import get_k_data_JQ
+from DataSource.auth_info import jq_login, logout
 from Experiment.CornerDetectAndAutoEmail.Sub import genStkIdxPicForQQ, genStkPicForQQ
+from Function.SeaSelect.gen_pic import gen_stk_sea_select_pic
+from SDK.Gen_Stk_Pic_Sub import gen_hour_macd_values
 
 from SDK.SendMsgByQQ.QQGUI import send_qq
 from SDK.SendMsgByQQ.SendPicByQQ import send_pic_qq
 
 from pylab import *
 from SDK.MyTimeOPT import get_current_date_str, add_date_str
+
+
+def download_stk_list_day_data(stk_list):
+    """
+    给定stk_list,下载日期数据,以tuple的形式返回
+    :return:
+    """
+
+    jq_login()
+    stk_list_data = [(x, get_k_data_JQ(x)) for x in stk_list]
+    logout()
+
+    return stk_list_data
+
+
+def download_stk_list_hour_data(stk_list):
+    """
+    给定stk_list,下载半小时数据,以tuple的形式返回
+    :return:
+    """
+
+    jq_login()
+    stk_list_data = [(x, gen_hour_macd_values(x)) for x in stk_list]
+    logout()
+
+    return stk_list_data
+
+
+def week_macd_judge(df_day, stk_code='', debug=False):
+    """
+    判断周线macd的反转情况
+    :param df_day: df_day = get_k_data_JQ(stk_code, 800)
+    :param stk_code:
+    :return:
+    """
+    try:
+        if debug:
+            print('开始计算' + str(stk_code) + '的周线macd判断！')
+
+        # 计算周线数据
+        df_week = get_week_month_index_data(df_day, stk_code='')[0]
+
+        # 判断周线转折
+        return week_macd_stray_judge_sub(df_week, debug_plot=False)
+
+    except Exception as e:
+        print('周反转判断失败，原因：\n' + str(e))
+        return False
 
 
 def get_week_month_index_data(df_stk, stk_code=''):
@@ -65,7 +116,7 @@ def get_week_month_index_data(df_stk, stk_code=''):
     return df_week, df_month
 
 
-def sar_stray_judge(df_stk):
+def sar_stray_judge(df_stk, debug=False):
     """
     判断sar的反转情况，返回三种值
     -1, 0， 1
@@ -76,17 +127,21 @@ def sar_stray_judge(df_stk):
     :param df_stk:
     :return:
     """
-    df_tail = df_stk.tail(2).reset_index()
-    
-    if (df_tail.loc[1, 'SAR'] >= df_tail.loc[1, 'close']) & (df_tail.loc[0, 'SAR'] <= df_tail.loc[0, 'close']):
-        return -1
-    elif (df_tail.loc[1, 'SAR'] <= df_tail.loc[1, 'close']) & (df_tail.loc[0, 'SAR'] >= df_tail.loc[0, 'close']):
-        return 1
-    else:
+    try:
+        df_tail = df_stk.tail(2).reset_index()
+
+        if (df_tail.loc[1, 'SAR'] >= df_tail.loc[1, 'close']) & (df_tail.loc[0, 'SAR'] <= df_tail.loc[0, 'close']):
+            return -1
+        elif (df_tail.loc[1, 'SAR'] <= df_tail.loc[1, 'close']) & (df_tail.loc[0, 'SAR'] >= df_tail.loc[0, 'close']):
+            return 1
+        else:
+            return 0
+    except Exception as e:
+        print('sar反转判断失败，原因：\n' + str(e))
         return 0
 
 
-def week_macd_stray_judge(df_week, debug_plot=False):
+def week_macd_stray_judge_sub(df_week, debug_plot=False):
 
     """
     对周线macd反转进行判断
@@ -217,27 +272,29 @@ def cal_stk_p_level(c):
 
 def sea_select():
 
+    jq_login()
 
-    df_total = ts.get_stock_basics()
+    df_total = ts.get_stock_basics()[:200]
 
     # 过滤掉年龄小于四岁的
-    df_age_filter = df_total[df_total.apply(lambda x: int(str(x['timeToMarket'])[:4]) <= int(get_current_date_str()[:4]) - 4, axis=1)]
+    df_stk = df_total[df_total.apply(lambda x: int(str(x['timeToMarket'])[:4]) <= int(get_current_date_str()[:4])-4, axis=1)]
 
-    # 找出半小时线反转的股票
+    print('已过滤掉上市小于4年的股票！')
 
     # 根据week反转情况进行过滤，保留有反转的单位
-    df_age_filter_stray = list(df_age_filter.reset_index().apply(lambda x: (x['code'], week_macd_stray_judge(x['code'], towho)), axis=1))
+    df_stk['week_stray'] = df_stk.reset_index().apply(lambda x: week_macd_judge(x['code'], debug=True), axis=1)
+    df_stk = df_stk[list[df_stk['week_stray']]]
+    print('已过滤出周线macd转折的股票！')
 
-    # 过滤掉非反转的情况
-    df_age_f2 = list(filter(lambda x: x[1][0], df_age_filter_stray))
-
-    # 筛选小时线sar反转
-
+    # 找出半小时线反转的股票
+    df_stk['hour_stray'] = df_stk.reset_index().apply(lambda x: sar_stray_judge(gen_hour_macd_values(x['code'], debug=True)[0]), axis=1)
+    df_stk = df_stk[df_stk.apply(lambda x: x['hour_stray'] == 1, axis=1)]
+    print('已过滤出半小时sar转折的股票！')
 
     # 增加stk的水平信息
-    df_level = [(x[0], x[1][1], cal_stk_p_level(x[1][1]['close'].values)) for x in df_age_f2]
+    # df_level = [(x[0], x[1][1], cal_stk_p_level(x[1][1]['close'].values)) for x in df_stk]
 
-    # # 按总体水平排序，筛选优异者
+    # 按总体水平排序，筛选优异者
     # df_level.sort(key=lambda x: x[2]['total_last'])
     # df_level = df_level[:math.floor(len(df_level)/3*2)]
     #
@@ -249,17 +306,20 @@ def sea_select():
     # df_level.sort(key=lambda x: x[2]['t30_last'], reverse=False)
     # df_level = df_level[:np.min([math.floor(len(df_level) / 3 * 2), 15])]
 
-
+    logout()
 
     # 打印信息
-    stk_list = [k[0] for k in df_level]
+    stk_list = [k[0] for k in df_stk.index]
 
     for stk in stk_list:
 
-
+        # 将选定的股票的走势图打印到本地
+        gen_stk_sea_select_pic(stk)
 
 
 if __name__ == '__main__':
+
+    sea_select()
 
     df = ts.get_stock_basics()
 
--- Function/SeaSelect/gen_pic.py
+++ Function/SeaSelect/gen_pic.py
@@ -19,25 +19,31 @@ from SDK.MyTimeOPT import get_current_date_str, get_current_datetime_str
 
 
 def gen_stk_sea_select_pic(stk_code):
-    # 保存路径
-    save_dir = sea_select_pic_dir + get_current_date_str() + '/'
 
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+    try:
 
-    # 准备 小时 和 日线 数据
-    df_hour = gen_hour_macd_values(stk_code)
-    df_day = get_k_data_JQ(stk_code, 800)
+        # 保存路径
+        save_dir = sea_select_pic_dir + get_current_date_str() + '/'
 
-    # 定义图片名称
-    file_name = stk_code + '.png'
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
 
-    # 生成小时图片
-    gen_hour_macd_pic_local(df_hour, stk_code, 'jq', '', save_dir + 'h_' + file_name)
-    gen_hour_index_pic_local(df_hour[0], stk_code, save_dir + 'h_idx_' + file_name)
-    gen_day_pic_local(df_day, stk_code, save_dir + 'd_' + file_name)
-    gen_w_m_macd_pic_local(df_day, stk_code, save_dir + 'wm_' + file_name)
-    gen_idx_pic_local(df_day, stk_code, save_dir + 'd_idx_' + file_name)
+        # 准备 小时 和 日线 数据
+        df_hour = gen_hour_macd_values(stk_code)
+        df_day = get_k_data_JQ(stk_code, 800)
+
+        # 定义图片名称
+        file_name = stk_code + '.png'
+
+        # 生成小时图片
+        gen_hour_macd_pic_local(df_hour, stk_code, 'jq', '', save_dir + 'h_' + file_name)
+        gen_hour_index_pic_local(df_hour[0], stk_code, save_dir + 'h_idx_' + file_name)
+        gen_day_pic_local(df_day, stk_code, save_dir + 'd_' + file_name)
+        gen_w_m_macd_pic_local(df_day, stk_code, save_dir + 'wm_' + file_name)
+        gen_idx_pic_local(df_day, stk_code, save_dir + 'd_idx_' + file_name)
+
+    except Exception as e:
+        print('生成股票走势图失败！原因：\n' + str(e))
 
 
 if __name__ == '__main__':
